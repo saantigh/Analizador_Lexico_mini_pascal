9e0add1e6d6bd21f3e6c714389997ad90931d910
juanesdevv <je772718@gmail.com>
2025-03-05 08:30:58 -0500

    Reformat

--- src/simple_tokens.py
+++ src/simple_tokens.py
@@ -1,25 +1,25 @@
 # Regular expression for a simple tokens
 
-t_ASIGNATION    = r':='      # Ejemplo: operador de asignación en Pascal
-t_PLUS          = r'\+'
-t_MINUS         = r'-'
-t_TIMES         = r'\*'
-t_DIVIDE        = r'/'
-t_LESS          = r'<'
-t_LESSEQUAL     = r'<='
-t_GREATER       = r'>'
-t_GREATEREQUAL  = r'>='
-t_EQUAL         = r'='
-t_DISTINT       = r'<>'
-t_SEMICOLON     = r';'
-t_COMMA         = r','
-t_LPAREN        = r'\('
-t_RPAREN        = r'\)'
-t_LBRACKET      = r'\['
-t_RBRACKET      = r'\]'
-t_COLON         = r':'
-t_DOT           = r'\.'
-t_DOBLEDOT      = r'\.\.'
+t_ASIGNATION = r":="  # Ejemplo: operador de asignación en Pascal
+t_PLUS = r"\+"
+t_MINUS = r"-"
+t_TIMES = r"\*"
+t_DIVIDE = r"/"
+t_LESS = r"<"
+t_LESSEQUAL = r"<="
+t_GREATER = r">"
+t_GREATEREQUAL = r">="
+t_EQUAL = r"="
+t_DISTINT = r"<>"
+t_SEMICOLON = r";"
+t_COMMA = r","
+t_LPAREN = r"\("
+t_RPAREN = r"\)"
+t_LBRACKET = r"\["
+t_RBRACKET = r"\]"
+t_COLON = r":"
+t_DOT = r"\."
+t_DOBLEDOT = r"\.\."
 
 # Regla para ignorar espacios y tabulaciones
-t_ignore = ' \t'
+t_ignore = " \t"
