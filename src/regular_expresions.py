6d1d1b99efa62beda56d1bcb62cb034c26fc7d57
saantigh <santi.gonzalezhincapie@gmail.com>
2025-03-05 07:59:31 -0500

    Se agregan las expresiones para tokens simples

--- src/regular_expresions.py
+++ src/regular_expresions.py
@@ -1,10 +1,26 @@
-print(
-    "Hello World"
-)
+# Regular expression for a simple tokens
 
-print("Ivan Barrios Barinas")
-print("hiauehiwufoaiwgfrla")
-O=67
-n=4
+t_ASIGNATION    = r':='      # Ejemplo: operador de asignación en Pascal
+t_PLUS          = r'\+'
+t_MINUS         = r'-'
+t_TIMES         = r'\*'
+t_DIVIDE        = r'/'
+t_LESS          = r'<'
+t_LESSEQUAL     = r'<='
+t_GREATER       = r'>'
+t_GREATEREQUAL  = r'>='
+t_EQUAL         = r'='
+t_DISTINT       = r'<>'
+t_SEMICOLON     = r';'
+t_COMMA         = r','
+t_LPAREN        = r'\('
+t_RPAREN        = r'\)'
+t_LBRACKET      = r'\['
+t_RBRACKET      = r'\]'
+t_COLON         = r':'
+t_DOBLECOLON    = r'::'
+t_DOT           = r'\.'
+t_DOBLEDOT      = r'\.\.'
 
-print()
+# Regla para ignorar espacios y tabulaciones
+t_ignore = ' \t'
